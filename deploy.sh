602a0ec48d619b052c61458404da123db6f385aa
Joan Domingo <joands@gmail.com>
2020-10-05 18:07:54 +0200

    Prepare to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 # Build client
 cd client && yarn && yarn build
 
+# Copy client build to backend directory
+cp -R dist ../server
+cd ..
+
+# Commit and push new changes
+git add .
+git commit -m "Prepare to deploy"
+git push
+
 
 # Push Server directory to Heroku
 git subtree push --prefix server heroku master
