2dc42122d3d8f6a1c40e46646c15b75de4ec2cb9
Joan Domingo <joands@gmail.com>
2020-10-05 12:25:22 +0200

    change url for websockets

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,2 @@
-# Server
-# cd server && yarn && yarn build
-# cp package.json lib
-# cp -R lib ../web
-
-# Client
-# cd ../client && yarn && yarn build
-# cp -R dist ../web/build
-
 # Push Server directory to Heroku
 git subtree push --prefix server heroku master
